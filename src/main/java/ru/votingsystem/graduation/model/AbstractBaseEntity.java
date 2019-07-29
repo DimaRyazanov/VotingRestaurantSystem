df053f568cb2d43713c7205d54052601c30e198a
Ryazan <ryazangaming@gmail.com>
2019-07-29 17:10:37 +0500

    Add Abstract base entity (field Id - auto generate) Add dependencies in pow (Spring context, Hibernate, Spring data jpa)

---
+++ src/main/java/ru/votingsystem/graduation/model/AbstractBaseEntity.java
@@ -0,0 +1,53 @@
+package ru.votingsystem.graduation.model;
+import org.springframework.data.domain.Persistable;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@MappedSuperclass
+@Access(AccessType.FIELD)
+public abstract class AbstractBaseEntity implements Persistable<Integer> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Integer id;
+
+    public AbstractBaseEntity() {
+    }
+
+    public AbstractBaseEntity(Integer id) {
+        this.id = id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return id == null;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ":" + id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractBaseEntity that = (AbstractBaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
