26e09adf344dbdf2d57ad5c52d52a63c0772d6d2
Ryazan <ryazangaming@gmail.com>
2019-07-31 16:55:58 +0500

    Profile rest controller

--- src/main/java/ru/votingsystem/graduation/util/ValidationUtil.java
+++ src/main/java/ru/votingsystem/graduation/util/ValidationUtil.java
@@ -31,4 +31,12 @@ public class ValidationUtil {
             throw new IllegalArgumentException(entity + " must be new (id = null)");
         }
     }
+
+    public static void assureIdConsistent(AbstractBaseEntity entity, int id) {
+        if (entity.isNew()) {
+            entity.setId(id);
+        }else if (entity.getId() != id) {
+            throw new IllegalArgumentException(entity + "must be with id=" + id);
+        }
+    }
 }
---
+++ src/main/java/ru/votingsystem/graduation/web/ProfileRestController.java
@@ -0,0 +1,39 @@
+package ru.votingsystem.graduation.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.votingsystem.graduation.model.User;
+import ru.votingsystem.graduation.service.UserService;
+import ru.votingsystem.graduation.util.SecurityUtil;
+import ru.votingsystem.graduation.util.ValidationUtil;
+
+@RestController
+@RequestMapping(ProfileRestController.REST_URL)
+public class ProfileRestController {
+    static final String REST_URL = "rest/profile";
+
+    private UserService userService;
+
+    public ProfileRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public User get() {
+        return userService.get(SecurityUtil.authUserId());
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete() {
+        userService.delete(SecurityUtil.authUserId());
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody User user) {
+        ValidationUtil.assureIdConsistent(user, SecurityUtil.authUserId());
+        userService.update(user);
+    }
+}
